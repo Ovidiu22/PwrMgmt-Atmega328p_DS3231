930cf4c3e11870516bce55bbb899b84ce176c47c
Ovidiu22 <75970114+Ovidiu22@users.noreply.github.com>
2022-12-30 10:58:04 +0100

    Revert "Interrupt implementation"
    This reverts commit 760afa1a81f6e8113e0d7f7fea7c8747547d7ad9.

--- src/main.c
+++ src/main.c
@@ -18,41 +18,54 @@
 #include <avr/wdt.h>
 
 void setRegistersAlarm1(void);
-void setAlarm1(void);
-uint8_t readStatusAlarm1(void);
+void displayRegistersAlarm1(void);
+void blink_LED_PORTB(uint8_t, uint8_t);
 uint8_t dec2bcd(uint8_t);
 
 int main (void)
 {
-	uint8_t st_reg = 0;
-	//initLCD();
-	//ds3231_basic_init();
+	ds3231_time_t t;
+	
+	initLCD();
+	ds3231_basic_init();
 	interruptConfig();
 
+	/* Set alarm */
+	setRegistersAlarm1();
+	
+	i2c_start((DS3231_ADDRESS<<1)+I2C_WRITE);
+	i2c_write(DS3231_REG_CONTROL);
+	i2c_write(0x5);		// Set bit 0 and 2 (alarm 1 and interrupt)
 
-	
-	
-	/* Main loop */
+	enable_interrupts();
+	//displayRegistersAlarm1();
+	/* Read and display current time */
 	while(1)
 	{
-		/* Set alarm 1*/
-		setRegistersAlarm1();
-		setAlarm1();
-			
+#if 1		
 		i2c_start((DS3231_ADDRESS<<1)+I2C_WRITE);
 		i2c_write(DS3231_REG_STATUS);
-		i2c_write(0x0);
+		/* Send START condition with SLA+R */
+		i2c_rep_start((DS3231_ADDRESS<<1)+I2C_READ);
+		/* Receive data */
+		uint8_t st_reg = i2c_readNak();
 		
-// 		st_reg = readStatusAlarm1();
-// 		if ((st_reg & 0x01) > 0)
-// 		{
-// 			
-// 		}
-		_delay_ms(2000);
+		ds3231_basic_get_time(&t);
+		updateLCDScreen(1, "Alarm flag: ", (st_reg & 0x01), "NONE");
+		updateLCDScreen(2, "Hour: ", t.hour, "NONE");
+		updateLCDScreen(3, "Minutes:", t.minute, "NONE");
+		updateLCDScreen(4, "Second:", t.second, "NONE");
 		
-		/* Power management */
-		//enable_interrupts();
-		//enterSleep();
+		if ((st_reg & 0x01) > 0)
+		{
+			//_delay_ms(200);
+			i2c_start((DS3231_ADDRESS<<1)+I2C_WRITE);
+			i2c_write(DS3231_REG_STATUS);
+			i2c_write(0x0);
+			
+		}
+#endif		
+		
 	}
 	return 0;
 }
@@ -86,13 +99,27 @@ Description:wakes up MCU when an external interrupt on pin PIND2 occurs
 ******************************************************************** */
 ISR(INT0_vect)
 {
-	//cli();
-	_delay_ms(10);
+	cli();
+	blink_LED_PORTB(PINB0, 2);
+	//_delay_ms(100);
+	sei();
+}
+
+void blink_LED_PORTB(uint8_t pinNumber, uint8_t times)
+{
+	DDRB |= (1 << pinNumber);
+	for (uint8_t i = 0; i < times; i++)
+	{
+		PORTB |= ( 1 << pinNumber );
+		_delay_ms(100);
+		PORTB &= ~( 1 << pinNumber );
+		_delay_ms(100);
+	}
 }
 
 void setRegistersAlarm1(void)
 {
-	/*------- Alarm 1 ---------- */
+/*------- Alarm 1 ---------- */
 	i2c_start((DS3231_ADDRESS<<1)+I2C_WRITE);
 	i2c_write(DS3231_REG_ALARM1_SECOND);
 	i2c_write(dec2bcd(7));	// Seconds in dec
@@ -110,21 +137,40 @@ void setRegistersAlarm1(void)
 	i2c_write(128);
 }
 
-void setAlarm1(void)
+void displayRegistersAlarm1(void)
 {
+	/*------- Alarm 1 ---------- */
 	i2c_start((DS3231_ADDRESS<<1)+I2C_WRITE);
-	i2c_write(DS3231_REG_CONTROL);
-	i2c_write(0x5);		// Set bit 0 and 2 (alarm 1 and interrupt)
-}
-
-uint8_t readStatusAlarm1(void)
-{
-	i2c_start((DS3231_ADDRESS<<1)+I2C_WRITE);
-	i2c_write(DS3231_REG_STATUS);
+	i2c_write(DS3231_REG_ALARM1_SECOND);
 	/* Send START condition with SLA+R */
 	i2c_rep_start((DS3231_ADDRESS<<1)+I2C_READ);
 	/* Receive data */
-	uint8_t st_reg = i2c_readNak();
+	uint8_t A1M1 = i2c_readNak();
 	
-	return st_reg;
+	i2c_start((DS3231_ADDRESS<<1)+I2C_WRITE);
+	i2c_write(DS3231_REG_ALARM1_MINUTE);
+	/* Send START condition with SLA+R */
+	i2c_rep_start((DS3231_ADDRESS<<1)+I2C_READ);
+	/* Receive data */
+	uint8_t A1M2 = i2c_readNak();
+	
+	i2c_start((DS3231_ADDRESS<<1)+I2C_WRITE);
+	i2c_write(DS3231_REG_ALARM1_HOUR);
+	/* Send START condition with SLA+R */
+	i2c_rep_start((DS3231_ADDRESS<<1)+I2C_READ);
+	/* Receive data */
+	uint8_t A1M3 = i2c_readNak();
+	
+	i2c_start((DS3231_ADDRESS<<1)+I2C_WRITE);
+	i2c_write(DS3231_REG_ALARM1_WEEK);
+	/* Send START condition with SLA+R */
+	i2c_rep_start((DS3231_ADDRESS<<1)+I2C_READ);
+	/* Receive data */
+	uint8_t A1M4 = i2c_readNak();
+	
+	updateLCDScreen(1, "A1M1: ", A1M1, "NONE");
+	updateLCDScreen(2, "A1M2: ", A1M2, "NONE");
+	updateLCDScreen(3, "A1M3: ", A1M3, "NONE");
+	updateLCDScreen(4, "A1M4: ", A1M4, "NONE");
+
 }
